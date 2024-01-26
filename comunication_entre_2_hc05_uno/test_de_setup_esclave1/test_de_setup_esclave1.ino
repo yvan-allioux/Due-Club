76f6c327778a6145d22a0f22866abdb5c66457aa
yvan-allioux <yvan.allioux.pro@gmail.com>
2024-01-26 16:59:40 +0100

    LE BT MARCHE C UNE DINGUERIE

--- comunication_entre_2_hc05_uno/test_de_setup_esclave1/test_de_setup_esclave1.ino
+++ comunication_entre_2_hc05_uno/test_de_setup_esclave1/test_de_setup_esclave1.ino
@@ -1,11 +1,39 @@
+//ESCLAVE
+
 #include <SoftwareSerial.h>
 
 SoftwareSerial bluetooth(2, 3); // Définir les broches TX (2) et RX (3) pour le module HC-05
 int ledPin = 13; // Pin pour la LED
+const int hc05_at_control_pin = 10;
 
 void setup() {
+  Serial.begin(9600);
+  pinMode(hc05_at_control_pin, OUTPUT);
+  digitalWrite(hc05_at_control_pin, HIGH); // Activer le mode AT
+  delay(1000); // Attendre que le module soit prêt
+
   pinMode(ledPin, OUTPUT);
   bluetooth.begin(38400); // Initialise la communication série avec le module HC-05
+
+  //sendATCommand("AT+RESET");
+  //delay(1000);
+  // Envoyez les commandes AT ici
+  sendATCommand("AT"); // Vérifiez la communication avec le module 
+  sendATCommand("AT+ORGL");
+
+  sendATCommand("AT+NAME=ESCLAVE"); //a tester
+  sendATCommand("AT+NAME?");
+
+  sendATCommand("AT+RMAAD");
+  sendATCommand("AT+ROLE=0"); // Définir le module en mode esclave
+  sendATCommand("AT+UART=38400,0,0"); // Configurer la vitesse en bauds
+
+  sendATCommand("AT+PSWD=1234"); // Définir le mot de passe
+  // Désactiver le mode AT
+  digitalWrite(hc05_at_control_pin, LOW);
+  sendATCommand("AT+RESET");
+  delay(1000);
+  Serial.println("end setup");
 }
 
 void loop() {
@@ -21,4 +49,18 @@ void loop() {
       }
     }
   }
+}
+
+
+void sendATCommand(const String& command) {
+  Serial.println("send to hc05 : " + command);
+  bluetooth.println(command); // Envoyer la commande AT
+  delay(1000); // Attendre un peu pour la réponse
+
+  // Lire et afficher la réponse
+  while (bluetooth.available()) {
+    char c = bluetooth.read();
+    Serial.write(c);
+  }
+  Serial.println("---");
 }

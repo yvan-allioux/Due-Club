4cca5f2430086c88f2b98c0796c36ae817773298
uhartegr <remi.uh@gmail.com>
2024-01-26 18:32:03 +0100

    Clean code and add comment

--- LoudSensor_Stick_Bar/LoudSensor_Stick_Bar.ino
+++ LoudSensor_Stick_Bar/LoudSensor_Stick_Bar.ino
@@ -1,4 +1,3 @@
-
 // Led stick
 #include "Adafruit_NeoPixel.h"
 #ifdef __AVR__
@@ -9,13 +8,14 @@
 #include <Grove_LED_Bar.h>
 
 // Led stick
-#define PIN 5
+#define PIN_LED_STICK 5
 #define NUMPIXELS 10
 
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel led_stick = Adafruit_NeoPixel(NUMPIXELS, PIN_LED_STICK, NEO_GRB + NEO_KHZ800);
 
 // Led Bar
-Grove_LED_Bar bar(7, 6, 0); // Clock pin, Data pin, Orientation
+Grove_LED_Bar led_bar(7, 6, 0); // Clock pin, Data pin, Orientation
+int led_bar_value = 1;
 
 // Led
 #define LED_RED 2
@@ -24,123 +24,113 @@ Grove_LED_Bar bar(7, 6, 0); // Clock pin, Data pin, Orientation
 #define LED_WHITE 8
 
 int leds[] = {LED_RED, LED_GREEN, LED_BLUE, LED_WHITE};
-int numLeds = sizeof(leds) / sizeof(leds[0]);
-int barValue = 1;
+int led_length = sizeof(leds) / sizeof(leds[0]);
+
 void setup()
 {
 
 // Led Stick
 #if defined(__AVR_ATtiny85__)
-    if (F_CPU == 16000000)
-        clock_prescale_set(clock_div_1);
+  if (F_CPU == 16000000)
+    clock_prescale_set(clock_div_1);
 #endif
-    pixels.begin();
+  Serial2.begin(9600); // Communication with Phone
+  
+  // Led Stick
+  led_stick.begin();
+  
+  // Led Bar
+  led_bar.begin();
 
-    // Led Bar
-    Serial.begin(9600);
-    Serial2.begin(9600); // Utiliser Serial2 pour la communication avec le téléphone (RX2, TX2)
-    bar.begin();
-
-    // Led
-    for (int i = 0; i < numLeds; i++)
-    {
-        pinMode(leds[i], OUTPUT);
-    }
+  // Led
+  for (int i = 0; i < led_length; i++)
+  {
+    pinMode(leds[i], OUTPUT);
+  }
 }
 
 void loop()
 {
-    // Check for commands received on RX2
-    if (Serial2.available())
-    {
-        char command = Serial2.read();
-        handleCommand(command);
-    }
+  // Get Phone commands
+  if (Serial2.available())
+  {
+    char command = Serial2.read(); // Get Phone commands
+    handleCommand(command);       // Handle Phone commands
+  }
 
-    // Set the LED bar based on the loudness
-    bar.setBits((1 << barValue) - 1);
+  // Set the LED bar based on the loudness
+  led_bar.setBits((1 << led_bar_value) - 1);
 
-    // Led Stick
-    // Set the color based on the current iteration
-    int red = random(256);
-    int green = random(256);
-    int blue = random(256);
-
-    pixels.setBrightness(random(5, 5 + (25 * barValue)));
-
-    // Set the color based on random values
-    pixels.setPixelColor(random(NUMPIXELS), pixels.Color(red, green, blue));
-    pixels.show();
+  // Led Stick
+  // Set the color based on the current iteration
+  int red = random(256);
+  int green = random(256);
+  int blue = random(256);
+  // Set the brightness based on the loudness
+  led_stick.setBrightness(random(5, 5 + (25 * led_bar_value)));
+  // Set the color based on random values
+  led_stick.setPixelColor(random(NUMPIXELS), led_stick.Color(red, green, blue));
+  led_stick.show();
 }
 
-// Function to handle commands received on RX2
+// Function to handle commands received on Phone
 void handleCommand(char command)
 {
-    switch (command)
+  switch (command)
+  {
+  case 'B':
+    // Turn off all leds
+    for (int i = 0; i < led_length; i++)
     {
-    case 'B':
-        // Éteindre led
-        for (int i = 0; i < numLeds; i++)
-        {
-            digitalWrite(leds[i], LOW);
-        }
-        bar.setBits(0);
-
-        break;
-    case 'A':
-        // Allumer led
-        for (int i = 0; i < numLeds; i++)
-        {
-            digitalWrite(leds[i], HIGH);
-        }
-        break;
-    case 'C':
-        // Allumer tout
-        if (barValue < 10)
-        {
-            barValue = barValue + 1;
-
-            if (barValue < 3)
-            {
-                Serial2.write("1");
-            }
-            else if (barValue < 6)
-            {
-                Serial2.write("2");
-            }
-            else
-            {
-                Serial2.write("3");
-            }
-        }
-        else
-        {
-            Serial2.write("4");
-        }
-        break;
-    case 'D':
-        // Allumer tout
-        if (barValue > 1)
-        {
-            barValue = barValue - 1;
-
-            if (barValue < 3)
-            {
-                Serial2.write("1");
-            }
-            else if (barValue < 6)
-            {
-                Serial2.write("2");
-            }
-            else
-            {
-                Serial2.write("3");
-            }
-        }
-        else
-        {
-            Serial2.write("0");
-        }
-        break;
+      digitalWrite(leds[i], LOW);
     }
+    break;
+  case 'A':
+    // Turn on all leds
+    for (int i = 0; i < led_length; i++)
+    {
+      digitalWrite(leds[i], HIGH);
+    }
+    break;
+  case 'C':
+    // Increase light level
+    if (led_bar_value < 10) // 10 is the maximum
+    {
+      led_bar_value = led_bar_value + 1;
+      level();
+    }
+    else // Maximum reached
+    {
+      Serial2.write("4"); // Send 4 to master
+    }
+    break;
+  case 'D':
+    // Decrease light level
+    if (led_bar_value > 0) // 0 is the minimum
+    {
+      led_bar_value = led_bar_value - 1;
+      level();
+    }
+    else // Minimum reached
+    {
+      Serial2.write("0"); // Send 0 to master
+    }
+    break;
+  }
+}
+
+void level()
+{
+  if (led_bar_value < 3)
+  {
+    Serial2.write("1"); // Send 1 to master
+  }
+  else if (led_bar_value < 6)
+  {
+    Serial2.write("2"); // Send 2 to master
+  }
+  else
+  {
+    Serial2.write("3"); // Send 3 to master
+  }
 }

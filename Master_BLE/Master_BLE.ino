6b25674b07caf3ac1207165cd6c3cd4ca62f2a3c
uhartegr <remi.uh@gmail.com>
2024-01-26 18:02:08 +0100

    Clean code and add comment

--- Master_BLE/Master_BLE.ino
+++ Master_BLE/Master_BLE.ino
@@ -1,28 +1,21 @@
 void setup() {
-  Serial1.begin(9600); // Communication avec le téléphone (maître)
-  Serial2.begin(9600); // Communication avec l'esclave
-  Serial3.begin(9600); // Communication avec Musique
-  Serial.begin(9600); // Communication avec l'esclave
+  Serial1.begin(9600); // Communication with Light
+  Serial2.begin(9600); // Communication with Phone
+  Serial3.begin(9600); // Communication with Sound
 }
 
 void loop() {
-  // Lire les données du téléphone (maître) et les envoyer à l'esclave
+
+  // Send Light info to Phone
   if (Serial1.available()) {
-    char data = Serial1.read();
-    Serial2.write(data);
+    char data = Serial1.read(); // Get Light data
+    Serial2.write(data); // Send to phone
   }
 
-  // Lire les données de l'esclave et les envoyer au téléphone (maître)
+  // Send Phone commands to Light and Sound
   if (Serial2.available()) {
-    char data = Serial2.read();
-    Serial1.write(data);
-  }
-  
-  if (Serial3.available()) {
-    char data = Serial3.read();
-    Serial.write(data);
-    Serial1.write(data);
-    Serial2.write(data);
-    Serial3.write(data);
+    char data = Serial2.read(); // Get Phone data
+    Serial1.write(data); // Send to Light
+    Serial3.write(data); // Send to Sound
   }
 }
